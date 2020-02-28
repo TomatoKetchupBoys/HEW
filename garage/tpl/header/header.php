2869db64358a6e6ab9a4036ba1e429c8083117c5
Kiharaten <sukerock324@gmail.com>
2020-02-28 18:47:42 +0900

    ホームに追加時のタイトルをGARAGEに変更

--- garage/tpl/header/header.php
+++ garage/tpl/header/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="viewport-fit=cover,user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-title" content="テストサイト">
+    <meta name="apple-mobile-web-app-title" content="GARAGE">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/howl.jpg">
 
     <link rel="stylesheet" href="./css/reset.css">
