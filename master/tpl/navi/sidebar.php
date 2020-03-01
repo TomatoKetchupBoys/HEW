60878a67597b3c1a93960a37bcdf21eca55846a2
Tatsuro Oshima <53423446+napthenap@users.noreply.github.com>
2020-03-02 00:27:57 +0900

    軽微な修正

--- master/tpl/navi/sidebar.php
+++ master/tpl/navi/sidebar.php
@@ -11,19 +11,19 @@
         <ul class="nav">
             <li>メタテーブル
                 <li class="nav-item active  ">
-                    <a class="nav-link" href="./idex.php?currentpage=refarence_time">
+                    <a class="nav-link" href="./index.php?currentPage=refarence_time">
                         <i class="material-icons">dashboard</i>
                         <p>基準時間</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=intervals">
+                    <a class="nav-link" href="./index.php?currentPage=intervals">
                         <i class="material-icons">person</i>
                         <p>繰り返し間隔</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=usage_statuses">
+                    <a class="nav-link" href="./index.php?currentPage=usage_statuses">
                         <i class="material-icons">content_paste</i>
                         <p>利用状態</p>
                     </a>
@@ -31,13 +31,13 @@
             </li>
             <li>マスターテーブル
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=parking">
+                    <a class="nav-link" href="./index.php?currentPage=parking">
                         <i class="material-icons">library_books</i>
                         <p>駐車場</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=parking_positions">
+                    <a class="nav-link" href="./index.php?currentPage=parking_positions">
                         <i class="material-icons">bubble_chart</i>
                         <p>駐車位置</p>
                     </a>
@@ -45,25 +45,25 @@
             </li>
             <li>ユーザーテーブル
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=members">
+                    <a class="nav-link" href="./index.php?currentPage=members">
                         <i class="material-icons">location_ons</i>
                         <p>会員アカウント</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=guests">
+                    <a class="nav-link" href="./index.php?currentPage=guests">
                         <i class="material-icons">notifications</i>
                         <p>ゲストアカウント</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./index.php?currentpage=partners">
+                    <a class="nav-link" href="./index.php?currentPage=partners">
                         <i class="material-icons">language</i>
                         <p>提携先アカウント</p>
                     </a>
                 </li>
                 <li class="nav-item active-pro ">
-                    <a class="nav-link" href="./index.php?currentpage=partners_children">
+                    <a class="nav-link" href="./index.php?currentPage=partners_children">
                         <i class="material-icons">unarchive</i>
                         <p>提携先子アカウント</p>
                     </a>
