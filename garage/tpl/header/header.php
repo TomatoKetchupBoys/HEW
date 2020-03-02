c08ceee1f336bee16e1ddce63b26986c9835e0f1
root <root@instance-1.us-central1-a.c.shaped-entropy-244114.internal>
2020-03-02 04:51:34 +0000

    changing title

--- garage/tpl/header/header.php
+++ garage/tpl/header/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="viewport-fit=cover,user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-title" content="テストサイト">
+    <meta name="apple-mobile-web-app-title" content="ON TIME">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/howl.jpg">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js"></script>
 
